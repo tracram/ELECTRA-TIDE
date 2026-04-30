d883e7015eabef803893980fdaa5db38bb4045f9
tracram <t.racoupeau.30@eigsi.fr>
2026-04-30 15:50:11 +0200

    Refactor pedal reading and improve code structure
    Refactor pedal reading logic into a separate function and adjust delay.

--- pedale.cpp
+++ pedale.cpp
@@ -1,28 +1,26 @@
-#include <Arduino.h>
-#include liquidcrystal_I2C.h
-#include Wire.h 
+
 
 //code pour simuler une pedale avec un potentiometre
 const int pedalPin = A0; // Pin du potentiomètre
-int pedalRaw = 0;
-float pedalPercent = 0.0;
+
 
 const int deadZone = 10;
 const int maxPedalValue = 1023;
+
+void pedalReadPercent() {
+    pedalRaw = analogRead(pedalPin);
+    raw=constrain(pedalRaw, deadZone, maxPedalValue);
+    int percent=map(raw, deadZone, maxPedalValue, 0, 100);
+    return percent;
+}
 
 void setup() {
     Serial.begin(115200);
 }
 
 void loop() {
-    pedalRaw = analogRead(pedalPin);
-    if (pedalRaw < deadZone) {
-        pedalRaw = 0;
-    } else if (pedalRaw > maxPedalValue - deadZone) {
-        pedalRaw = maxPedalValue;
-    }
-    pedalPercent = ((float)pedalRaw / (float)maxPedalValue) * 100.0;
-    Serial.println(pedalPercent);
+    int pedalPercent = pedalReadPercent();
+    int pedalRaw = analogRead(pedalPin);
 //debug :
 
     Serial.print("Pedal Raw: ");
@@ -31,6 +29,6 @@ void loop() {
     Serial.print(pedalPercent);
     Serial.println("%");
 
-    delay(100);
+    delay(500);
 
 }
